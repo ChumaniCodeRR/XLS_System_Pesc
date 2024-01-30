3c098a2cf10630c7db439060f3c9ba7127fbe61f
Chumani Mqulo <C.Mqulo@ru.ac.za>
2024-01-30 20:06:44 +0200

    Add project files.

---
+++ Downloading_XLS_PeSec/JSExlsfileDownload.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Downloading_XLS_PeSec
+{
+    partial class JSExlsfileDownload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblDownloaded = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(274, 268);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(82, 41);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(171, 139);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(481, 22);
+            this.txtURL.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(595, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Generate XLS, and downlaod file from JSE website ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(114, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(117, 219);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(535, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(171, 178);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(21, 16);
+            this.lblstatus.TabIndex = 6;
+            this.lblstatus.Text = "??";
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(362, 268);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(82, 41);
+            this.btnPause.TabIndex = 7;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(450, 268);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(82, 41);
+            this.btnResume.TabIndex = 8;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Percentage";
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(171, 325);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(10, 16);
+            this.lblPercentage.TabIndex = 10;
+            this.lblPercentage.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Speed";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(171, 365);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(10, 16);
+            this.lblSpeed.TabIndex = 12;
+            this.lblSpeed.Text = ".";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 401);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Downloaded File";
+            // 
+            // lblDownloaded
+            // 
+            this.lblDownloaded.AutoSize = true;
+            this.lblDownloaded.Location = new System.Drawing.Point(174, 400);
+            this.lblDownloaded.Name = "lblDownloaded";
+            this.lblDownloaded.Size = new System.Drawing.Size(10, 16);
+            this.lblDownloaded.TabIndex = 14;
+            this.lblDownloaded.Text = ".";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(331, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Select File to Database";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(515, 436);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // JSExlsfileDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDownloaded);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "JSExlsfileDownload";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblstatus;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblDownloaded;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Downloading_XLS_PeSec/JSExlsfileDownload.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using AltoHttp;
+using AltoHttp.BrowserIntegration.Chrome;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+
+namespace Downloading_XLS_PeSec
+{
+    public partial class JSExlsfileDownload : Form
+    {
+        HttpDownloader httpDownloader;
+
+        private Queue<string> _downloadUrls = new Queue<string>();
+
+        string MyConString = "Data Source=DESKTOP-C5R82QL\\SQLEXPRESS;Initial Catalog=JSE_Test; Integrated Security=true;";
+
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+
+        OpenFileDialog openfiledialog1 = new OpenFileDialog();
+
+        public JSExlsfileDownload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //_downloadUrls.Enqueue("https://clientportal.jse.co.za/downloadable-files?RequestNode=/YieldX/Derivatives/Docs_DMTM/20240126_D_Daily_MTM_Report.xls");
+            //downloadFile(_downloadUrls);
+            downloadFile();
+        }
+
+        private void HttpDownloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = (int)e.Progress;
+            lblPercentage.Text = $"{e.Progress.ToString("0.00")} %";
+            lblSpeed.Text = string.Format("{0} MB/s", (e.SpeedInBytes / 1024d / 1024d).ToString("0.00"));
+            lblDownloaded.Text = string.Format("{0} MB/s", (httpDownloader.TotalBytesReceived / 1024d / 1024d).ToString("0.00"));
+            lblstatus.Text = "Loading.................";
+
+        }
+
+        private void downloadFile()
+        {
+            // Starts the download
+            lblstatus.Text = "Downloading...";
+            btnGenerate.Enabled = false;
+            progressBar1.Visible = true;
+            lblDownloaded.Visible = true;
+
+            StringDownload();
+
+        }
+
+        public void StringDownload()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            WebClient client = new WebClient();
+
+            string _filePath = "";
+            string _fileToDownload = "";
+
+
+            client.DownloadProgressChanged += client_DownloadProgressChanged;
+            client.DownloadFileCompleted += client_DownloadFileCompleted;
+
+            int days = DateTime.DaysInMonth(2024, 01);
+            int countdec = DateTime.DaysInMonth(2023, 12);
+
+
+            for (int index = 0; index < days; index++)
+            {
+                _filePath = @"C:\Files\JSEDownloadedData_" + index + ".xls";
+
+                DateTime dateTime;
+                var year = 2024;
+                DateTime.TryParse(string.Format("1/1/{0}", year), out dateTime);
+
+                if (year == 2024) _fileToDownload = "https://clientportal.jse.co.za/downloadable-files?RequestNode=/YieldX/Derivatives/Docs_DMTM/2024010" + index + "_D_Daily_MTM_Report.xls";
+                else if (year == 2023)
+                {
+                    int month = 0;
+                    month++;
+                    _fileToDownload = "https://clientportal.jse.co.za/downloadable-files?RequestNode=/YieldX/Derivatives/Docs_DMTM/2023" + month + "" + index + "_D_Daily_MTM_Report.xls";
+                }
+
+                if (_fileToDownload != null)
+                {
+                    client.DownloadFile(new Uri(_fileToDownload), @_filePath);
+                }
+            }
+
+            lblstatus.Text = "Download Complete";
+            // btnGenerate.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                // handle error scenario
+                throw e.Error;
+            }
+            if (e.Cancelled)
+            {
+                // handle cancelled scenario
+            }
+
+            StringDownload();
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            /*if (httpDownloader != null)
+                httpDownloader.Pause();*/
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            /* if (httpDownloader != null)
+                 httpDownloader.Resume(); */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openfiledialog1.ShowDialog();
+            openfiledialog1.Filter = "allfiles|*.xls";
+            lblDownloaded.Text = openfiledialog1.FileName;
+
+            //  lblDownloaded
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt;
+            // OpenFileDialog dlg = new OpenFileDialog();
+            openfiledialog1.Filter = "Excel files | *.xls";
+
+            //if (openfiledialog1.ShowDialog() == DialogResult.OK)
+            //  {
+            string filePath = openfiledialog1.FileName;
+            string extension = Path.GetExtension(filePath);
+            string conStr, sheetName;
+
+            conStr = string.Empty;
+            switch (extension)
+            {
+
+                case ".xls": //Excel 97-03
+                    conStr = string.Format(Excel03ConString, filePath);
+                    break;
+
+                case ".xlsx": //Excel 07 to later
+                    conStr = string.Format(Excel07ConString, filePath);
+                    break;
+            }
+
+            //Read Data from the Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [Sheet1$]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+                    }
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection(MyConString))
+            {
+                using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                {
+                    //Set the database table name
+                    sqlBulkCopy.DestinationTableName = "dbo.DailyMTM";
+
+                    //[OPTIONAL]: Map the Excel columns with that of the database table
+
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        sqlBulkCopy.ColumnMappings.Add(i, i);
+                    }
+
+                    con.Open();
+                    sqlBulkCopy.WriteToServer(dt);
+                    con.Close();
+                }
+            }
+            //}
+        }
+    }
+}
